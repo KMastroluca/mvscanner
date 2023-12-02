79d344d919201e99e2124c954c7906a557c9891e
PThorpe92 <preston@unlockedlabs.org>
2023-12-02 16:16:31 -0500

    fix: POST error to backend with updated timestamp from user

--- scanui/src/api/API.ts
+++ scanui/src/api/API.ts
@@ -2,46 +2,46 @@ import { ServerResponse } from "../types/Models";
 
 
 
-export const GET = async (url:string):Promise<ServerResponse|undefined> => {
-   try {
-      const response = await fetch(url, {
-         method: "GET",
-         headers: {
-            accept: "application/json",
-            "Content-Type": "application/json",
-         },
-      });
+export const GET = async (url: string): Promise<ServerResponse | undefined> => {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
 
-      if (!response.ok) {
-         throw Error(response.statusText);
-      }
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
 
-      return await response.json();
-   } catch (err) {
-      console.error("GET ERROR:", err);
-      return undefined;
-   }
+    return await response.json();
+  } catch (err) {
+    console.error("GET ERROR:", err);
+    return undefined;
+  }
 };
 
 
-export const POST = async (url:string, payload:any):Promise<ServerResponse|undefined> => {
-   try {
-      const response = await fetch(url, {
-         method: "POST",
-         headers: {
-            accept: "application/json",
-            "Content-Type": "application/json",
-         },
-         body: JSON.stringify([payload]),
-      });
+export const POST = async (url: string, payload: any): Promise<ServerResponse | undefined> => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
-      if (!response.ok) {
-         throw Error(response.statusText);
-      }
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
 
-      return await response.json();
-   } catch (err) {
-      console.error("POST ERROR:", err);
-      return undefined;
-   }
+    return await response.json();
+  } catch (err) {
+    console.error("POST ERROR:", err);
+    return undefined;
+  }
 };
