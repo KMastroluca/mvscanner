e3b9f8ac61e3293327d67f38c0424f7dbaf22e0a
PThorpe92 <preston@unlockedlabs.org>
2023-12-02 21:14:08 -0500

    feat: add resident pod to model and views

--- scanui/src/types/Models.ts
+++ scanui/src/types/Models.ts
@@ -32,48 +32,48 @@
  * a resident and their location. As seen by the server.
  * */
 export interface STimestampResident {
-    [key:string]:string|number;
-    rfid:string;
-    name:string;
-    doc:string;
-    room:string;
-    unit:number;
-    timestampLeft:string;
-    location:number;
-    destinationLabel:string; // The idea is this comes from the Timestamp destinationId, and is resolved to a location name
+  [key: string]: string | number;
+  rfid: string;
+  name: string;
+  doc: string;
+  room: string;
+  unit: number;
+  timestampLeft: string;
+  location: number;
+  destinationLabel: string; // The idea is this comes from the Timestamp destinationId, and is resolved to a location name
 }
 
 
 export interface SResident {
-    [key:string]:string |number;
-    current_location:number;
-    rfid:string;
-    name:string;
-    doc:string;
-    room:string;
-    unit:number; // Unit ID
+  [key: string]: string | number;
+  current_location: number;
+  rfid: string;
+  name: string;
+  doc: string;
+  room: string; // Pod + room + bunk
+  unit: number; // Unit ID
 }
 
 
 
 export interface SLocation {
-    [key:string]:string|number;
-    id:number;
-    name:string;
+  [key: string]: string | number;
+  id: number;
+  name: string;
 }
 
 
 
 export interface STimestamp {
-    [key:string]:string|number|undefined;
-    rfid:string;
-    location:number;
-    time?:string;
+  [key: string]: string | number | undefined;
+  rfid: string;
+  location: number;
+  time?: string;
 }
 
 
-export interface ServerResponse  {
-    success:boolean;
-    message?:string;
-    data?:SResident[]|STimestamp[]|SLocation[];
+export interface ServerResponse {
+  success: boolean;
+  message?: string;
+  data?: SResident[] | STimestamp[] | SLocation[];
 }
